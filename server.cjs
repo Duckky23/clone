8aba0da85f86eb987c96dbfce15de8ac69b47fa9
Duckky23 <ducnm@fordeercommerce.io>
2024-11-11 00:07:55 +0700

    cors

--- server.cjs
+++ server.cjs
@@ -5,7 +5,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: 'http://localhost:3000', // URL của React client
+    origin: ['http://localhost:3000', 'https://clone-six-coral.vercel.app'], // Chấp nhận từ cả localhost và vercel
     methods: ['GET', 'POST'],
   },
 });
